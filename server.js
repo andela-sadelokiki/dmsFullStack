7659dc765d2aad53ebcd03beea382f1c8f298c69
Susan <susan.adelokiki@andela.co>
2015-10-26 23:44:16 +0100

    Set server to listen on port

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 'use strict';
 
 var db = require('./config/config.js');
-var app = require('./config/express.js');
+var app = require('./config/express')();
+console.log("app", app)
 var port = process.env.PORT || 3030;
 app.listen(port);
 console.log('App up and running on port:' + port);
